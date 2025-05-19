a489b5adc90146ad435d8cf4af248d884fb0baf2
danielmr28 <00085920@uca.edu.sv>
2025-05-18 22:46:49 -0600

    Creacion entidad User

---
+++ UniStay/src/main/java/com/dog/controller/PostController.java
@@ -0,0 +1,47 @@
+package com.dog.unistay.controller;
+
+import com.dog.unistay.dto.response.PostResponse;
+import com.dog.unistay.exception.PostNotFoundException;
+import com.dog.unistay.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/post")
+public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // GET /api/post
+    @GetMapping()
+    public ResponseEntity<List<PostResponse>> getPosts() {
+        List<PostResponse> posts = postService.findAll();
+
+        if (posts.isEmpty()) {
+            throw new PostNotFoundException("Post not found or empty list");
+        }
+        return ResponseEntity.ok(posts);
+    }
+
+    // GET /api/post/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponse> getPostById(@PathVariable int id) {
+        PostResponse post = postService.findById(id);
+        if (post == null) {
+            throw new PostNotFoundException("Post not found");
+        }
+        return ResponseEntity.ok(post);
+    }
+
+}
---
+++ UniStay/src/main/java/com/dog/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.dog.unistay.controller;
+
+import com.dog.unistay.dto.request.UserRequest;
+import com.dog.unistay.dto.request.UserUpdateRequest;
+import com.dog.unistay.dto.response.UserResponse;
+import com.dog.unistay.exception.UserNotFoundException;
+import com.dog.unistay.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // GET /api/user
+    @GetMapping()
+    public ResponseEntity<List<UserResponse>> getUser() {
+        List<UserResponse> users = userService.findAll();
+
+        if (users.isEmpty()) {
+            throw new UserNotFoundException("Users not found or empty list");
+        }
+        return ResponseEntity.ok(users);
+    }
+
+    // GET /api/user/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable int id) {
+        UserResponse user = userService.findById(id);
+        if (user == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    // POST /
+    @PostMapping("/")
+    public ResponseEntity<UserResponse> saveUser(@RequestBody @Valid UserRequest user) {
+        if (user == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(userService.save(user));
+    }
+
+    // PUT /
+    @PutMapping("/")
+    public ResponseEntity<UserResponse> updateUser(@RequestBody UserUpdateRequest user) {
+        UserResponse updateUser = userService.findById(user.getUserId());
+       if (updateUser == null) {
+           throw new UserNotFoundException("User not found");
+       }
+       return ResponseEntity.ok(updateUser);
+    }
+
+    // DELETE /{id}
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userService.delete(id);
+    }
+}
